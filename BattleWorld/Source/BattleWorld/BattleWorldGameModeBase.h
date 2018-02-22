d4cacfe642af06a2ad00ba063d00db5ed94c212d
Dheeju <dheerajguhan61@gmail.com>
2018-02-22 07:22:43 +0530

    Creating unreal & landscapes

---
+++ BattleWorld/Source/BattleWorld/BattleWorldGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BattleWorldGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLEWORLD_API ABattleWorldGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
